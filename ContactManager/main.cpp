27dc2e1d976646ead7ecb0b6eceff268d8d54506
Abdulwadud <abdulwadud.kola@gmail.com>
2024-04-12 16:58:01 -0500

    Initial Commit

---
+++ ContactManager/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ContactManager
+//
+//  Created by Abdulwadud Abdulkadir on 4/12/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
